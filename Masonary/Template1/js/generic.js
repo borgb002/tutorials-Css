eb386054ad4e9a8563fbea026b4087a0bc69018c
DabolB <Dabol_B>
2016-06-11 08:05:01 +0200

    Initialisation

---
+++ Masonary/Template1/js/generic.js
@@ -0,0 +1,20 @@
+﻿
+function sayHello() {
+    alert('saying hi!'); 
+}
+
+function longProcessOrSleep(ms) {
+
+    var dt = new Date();
+
+    dt.setTime(dt.getTime() + ms);
+
+    while (new Date().getTime() < dt.getTime()) {
+        document.getElementById('inner').innerHTML =
+            document.getElementById('inner').innerHTML + "<br/>" + new Date().getTime();
+    };
+
+}
+
+//longProcessOrSleep(5000);
+//sayHello();
